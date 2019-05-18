1c162a7955937d072287914943ef2eed9a628a48
daniell21 <daniellorente20@gmail.com>
2019-05-19 01:52:43 +0200

    imprimiendo jugadas y montos en pdf

--- invoice.php
+++ invoice.php
@@ -1,3 +1,17 @@
+<?php 
+session_start();
+
+
+
+
+$mysqli = new mysqli("localhost", "root", "", "ticketly");
+$query = "SELECT * FROM loteria";
+$result = mysqli_query($mysqli, $query);
+//$result = $mysqli->query("SELECT * AS _message FROM loteria");
+//$row = $result->fetch_assoc();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +21,10 @@
     <title>Ticket</title>
 </head>
 <body>
+
 <h1>CONSORCIO K.A SPORT. JULIA</h1>
+
+
 <p>New York Día</p> 
 <p>Real</p> 
 <p>Gana Más</p> 
@@ -16,5 +33,39 @@
 <p>Quiniela Pale</p> 
 <p>Nacional Noche</p> 
 <h2>Total:</h2>
+
+
+<table class="table table-striped jambo_table bulk_action">
+                <thead>
+                    <tr class="headings">
+                        <th class="column-title">Jugada </th>
+                        <th class="column-title">Monto </th>
+                        <th class="column-title no-link last"><span class="nobr"></span></th>
+                    </tr>
+                </thead>
+
+                
+<?php
+if (mysqli_num_rows($result) > 0) {
+    while($fila = mysqli_fetch_assoc($result)){
+        $id=$fila["id"];
+        $monto=$fila["monto"];
+        $jugada=$fila["jugada"];
+
+
+?>
+                
+
+                    <input type="hidden" value="<?php echo $id;?>" id="id<?php echo $id;?>">
+                    <input type="hidden" value="<?php echo $jugada;?>" id="jugada<?php echo $id;?>">
+                    <input type="hidden" value="<?php echo $monto;?>" id="monto<?php echo $id;?>">
+                    <tr class="even pointer">
+                        <td><?php echo $jugada;?></td>
+                        <td><?php echo $monto;?></td>
+                    </tr>
+                <?php
+    }}
+                ?>
+              </table>
 </body>
 </html>
