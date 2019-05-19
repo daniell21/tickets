f6d5f2f5a19649a6e47ff7e35330f6d1a7ac42e0
daniell21 <daniellorente20@gmail.com>
2019-05-19 03:07:44 +0200

    se añadio validaciones en loterias marcadas y si no hay datos. impresion pdf

--- invoice.php
+++ invoice.php
@@ -7,6 +7,8 @@ session_start();
 $mysqli = new mysqli("localhost", "root", "", "ticketly");
 $query = "SELECT * FROM loteria";
 $result = mysqli_query($mysqli, $query);
+$queryNombres = "SELECT * FROM loteriasnombre ORDER BY ID DESC LIMIT 1";
+$resultNombres = mysqli_query($mysqli, $queryNombres);
 //$result = $mysqli->query("SELECT * AS _message FROM loteria");
 //$row = $result->fetch_assoc();
 
@@ -24,16 +26,47 @@ $result = mysqli_query($mysqli, $query);
 
 <h1>CONSORCIO K.A SPORT. JULIA</h1>
 
+<?php
+if (mysqli_num_rows($resultNombres) > 0) {
+    $total = 0;
+    while($fila = mysqli_fetch_assoc($resultNombres)){
+        $id=$fila["id"];
+        $newYorkDia=$fila["NYD"];
+        $real=$fila["R"];
+        $ganaMas=$fila["GM"];
+        $loteka=$fila["L"];
+        $neWYorkNoche=$fila["NYN"];
+        $quinielaPale=$fila["QP"];
+        $nacionalNoche=$fila["NN"];
 
+    }
+
+?>
+<?php if ($newYorkDia){ ?>
 <p>New York Día</p> 
+<?php }?>
+<?php if ($real){ ?>
 <p>Real</p> 
-<p>Gana Más</p> 
+<?php }?>
+<?php if ($ganaMas){ ?>
+<p>Ganas Más</p> 
+<?php }?>
+<?php if ($loteka){ ?>
 <p>Loteka</p> 
+<?php }?>
+<?php if ($neWYorkNoche){ ?>
 <p>New York Noche</p> 
+<?php }?>
+<?php if ($quinielaPale){ ?>
 <p>Quiniela Pale</p> 
+<?php }?>
+<?php if ($nacionalNoche){ ?>
 <p>Nacional Noche</p> 
-<h2>Total:</h2>
+<?php }?>
 
+<?php } else {?>
+<h1>No hay Datos</h1>
+<?php }?>
 
 <table class="table table-striped jambo_table bulk_action">
                 <thead>
@@ -47,6 +80,7 @@ $result = mysqli_query($mysqli, $query);
                 
 <?php
 if (mysqli_num_rows($result) > 0) {
+    $total = 0;
     while($fila = mysqli_fetch_assoc($result)){
         $id=$fila["id"];
         $monto=$fila["monto"];
@@ -54,18 +88,20 @@ if (mysqli_num_rows($result) > 0) {
 
 
 ?>
-                
-
-                    <input type="hidden" value="<?php echo $id;?>" id="id<?php echo $id;?>">
-                    <input type="hidden" value="<?php echo $jugada;?>" id="jugada<?php echo $id;?>">
-                    <input type="hidden" value="<?php echo $monto;?>" id="monto<?php echo $id;?>">
+              
                     <tr class="even pointer">
                         <td><?php echo $jugada;?></td>
                         <td><?php echo $monto;?></td>
                     </tr>
                 <?php
-    }}
+    }
+            $total = $total + $monto;
+    
+}
+
                 ?>
               </table>
+              <?php if(isset($total)) { ?>
+              <p>Total:</p> <?php  echo $total;}?>
 </body>
 </html>
